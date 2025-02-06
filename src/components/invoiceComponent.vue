d582c465d09e16ff45a2ae4bb27f0101bf27a48e
David <davidstudiosco@gmail.com>
2025-02-06 22:54:48 +0100

    Add simple style

--- src/components/invoiceComponent.vue
+++ src/components/invoiceComponent.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>Invoice</h1>
-    <label for="">Subtotal</label>
-    <input type="number" v-model="subtotal">
+    <div class="subtotal">
+        <label for="">Subtotal</label>
+        <input type="number" v-model="subtotal">
+    </div>
 
     <h3>Total impuestos a pagar: {{ totalIva }} € ({{ iva }}%)</h3>
   </div>
@@ -19,5 +21,51 @@ import { computed, Ref, ref } from 'vue';
 </script>
 
 <style scoped>
+
+div {
+  max-width: 400px;
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  background-color: #f9f9f9;
+  text-align: center;
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+  color: #333;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+  color: #555;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  text-align: center;
+  margin-bottom: 15px;
+}
+
+h3 {
+  font-size: 18px;
+  color: #007bff;
+}
+
+.subtotal {
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    gap: 10px;
+}
 
 </style>
